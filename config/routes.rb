55c225ca84ff60d03096c2e045dded4537b14daa
obed300 <obamfo@st.ug.edu.gh>
2023-07-01 00:44:33 +0000

    Make changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-
-
   resources :users, only: %i[index show new edit] do
     resources :categories do
       resources :trades
@@ -14,9 +12,4 @@ Rails.application.routes.draw do
 
   root 'splash#index'
   get '/splash', to: 'splash#index'
-
-  # Define your additional application routes here
-
-  # For example, to define a custom greeting page route:
-  # get '/greeting', to: 'custom_pages#hello'
 end
